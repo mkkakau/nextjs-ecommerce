503177dd76c6260f546296d3856b5a5d1714611b
Melanie Akau <mkkakau@gmail.com>
2024-06-11 15:48:46 -1000

    Create dashboard cards

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,34 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function AdminDashboard() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"></div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <DashboardCard title="Sales" subtitle="test" body="body" />
+      <DashboardCard title="Sales" subtitle="test" body="body" />
+    </div>
+  );
+}
+type DashboardCardProps = {
+  title: string;
+  subtitle: string;
+  body: string;
+};
+function DashboardCard({ title, subtitle, body }: DashboardCardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{subtitle}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{body}</p>
+      </CardContent>
+    </Card>
   );
 }
